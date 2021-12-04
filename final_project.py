651ed90086303c1b41128cb1ba67cba93bc32903
Luke Nelson <64610511+lukenels10@users.noreply.github.com>
2021-12-04 14:09:23 -0800

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -23,6 +23,47 @@ alphas = {('A', 'A'): 0,
          ('T', 'G'): 110, 
          ('T', 'T'): 0}
 
+
+def read_file(input_file):
+    # Open input file and determine base_string_X with its indices and base_string_Y with its indices
+    txt = open(input_file, "r")
+
+    first_line = True
+    X_switch = True
+
+    base_string_X = None
+    base_string_Y = None
+    X_indices = []
+    Y_indices = []
+
+    for line in txt:
+        line = line.rstrip()
+
+        if first_line == True:
+            base_string_X = line
+            first_line = False
+
+        elif X_switch == True:
+            try:
+                line = int(line)
+                X_indices.append(line)
+            except:
+                base_string_Y = line
+                X_switch = False
+        else:
+            try:
+                Y_indices.append(int(line))
+            except:
+                break
+
+    txt.close()
+    
+    return {'base_string_X': base_string_X, 
+           'base_string_Y': base_string_Y, 
+           'X_indices': X_indices, 
+           'Y_indices': Y_indices}
+
+
 def string_generator(string: str, jk: list):
     """
     Gererate the stings from the input files
@@ -90,38 +131,11 @@ if __name__ == '__main__':
     input_file = sys.argv[1]
     output_file = 'output.txt'
     
-    # Open input file and determine base_string_X with its indices and base_string_Y with its indices
-    txt = open(input_file, "r")
-
-    first_line = True
-    X_switch = True
-
-    base_string_X = None
-    base_string_Y = None
-    X_indices = []
-    Y_indices = []
-
-    for line in txt:
-        line = line.rstrip()
-
-        if first_line == True:
-            base_string_X = line
-            first_line = False
-
-        elif X_switch == True:
-            try:
-                line = int(line)
-                X_indices.append(line)
-            except:
-                base_string_Y = line
-                X_switch = False
-        else:
-            try:
-                Y_indices.append(int(line))
-            except:
-                break
-
-    txt.close()
+    file_data = read_file(input_file)
+    base_string_X = file_data['base_string_X']
+    base_string_Y = file_data['base_string_Y']
+    X_indices = file_data['X_indices']
+    Y_indices = file_data['Y_indices']
     
     
     
