6932dc68a66233d9ef508dd24d8aa485c9dd9150
Luke Nelson <64610511+lukenels10@users.noreply.github.com>
2021-12-04 12:03:01 -0800

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -3,6 +3,8 @@ Project Name: CSCI 570 Final Project
 Author: Luke Nelson, Peter Argo
 """
 
+import sys
+
 
 def string_generator(string: str, jk: list):
     """
@@ -18,7 +20,64 @@ def string_generator(string: str, jk: list):
     return string_generator(new_string, jk[1::])
 
 
+def str_length_validation(base_string, full_string, indices):
+    full_str_length = len(full_string)
+    base_str_length = len(base_string)
+    j = len(indices)
+    
+    validation_length = (2 ** j) * base_str_length
+    
+    if full_str_length == validation_length:
+        print('TRUE')
+        print('Full String Length: ', full_str_length)
+        print('Validation Length: ', validation_length)
+    
+    else:
+        print('FALSE')
+        print('Full String Length: ', full_str_length)
+        print('Validation Length: ', validation_length)
+
+
 if __name__ == '__main__':
+    
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    
+    # Open input file and determine base_string_X with its indices and base_string_Y with its indices
+    txt = open(input_file, "r")
+
+    first_line = True
+    X_switch = True
+
+    base_string_X = None
+    base_string_Y = None
+    X_indices = []
+    Y_indices = []
+
+    for line in txt:
+        line = line.rstrip()
+
+        if first_line == True:
+            base_string_X = line
+            first_line = False
+
+        elif X_switch == True:
+            try:
+                line = int(line)
+                X_indices.append(line)
+            except:
+                base_string_Y = line
+                X_switch = False
+        else:
+            try:
+                Y_indices.append(int(line))
+            except:
+                break
+
+    txt.close()
+    
+    
+    
     s1 = "ACTG"     # "ACACTGACTACTGACTGGTGACTACTGACTGG"
     j = [3,6,1]
     s2 = "TACG"     # "TATTATACGCTATTATACGCGACGCGGACGCG"
